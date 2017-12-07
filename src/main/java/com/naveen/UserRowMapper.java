428fdc26d251d73ed8e08db7a50d62986a133eac
Naveen Kumar <kumar.naveen2989@gmail.com>
2017-12-07 18:50:16 +0530

    SpringBatchMySQLToXML commit

---
+++ src/main/java/com/naveen/UserRowMapper.java
@@ -0,0 +1,22 @@
+package com.naveen;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+public class UserRowMapper implements RowMapper<User> {
+
+	@Override
+	public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+		User user = new User();
+
+		user.setId(rs.getInt("id"));
+		user.setUsername(rs.getString("user_login"));
+		user.setPassword(rs.getString("password"));
+		user.setAge(rs.getInt("age"));
+
+		return user;
+	}
+
+}
