cacbfeecdb0c2cfa5ecbeb44f311e1515e7da360
Naveen Kumar <kumar.naveen2989@gmail.com>
2017-12-07 20:11:50 +0530

    SpringSecurityLimitLoginAttemptsExample

---
+++ src/main/java/com/naveen/users/dao/UserDetailsDao.java
@@ -0,0 +1,13 @@
+package com.naveen.users.dao;
+
+import com.naveen.users.model.UserAttempts;
+
+public interface UserDetailsDao {
+
+	void updateFailAttempts(String username);
+
+	void resetFailAttempts(String username);
+	
+	UserAttempts getUserAttempts(String username);
+
+}
